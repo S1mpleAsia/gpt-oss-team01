3c64b4f95b55f7154f7a6ed2536f6548f56755ff
duongvt <duongk65bkhn@gmail.com>
2025-08-24 08:58:11 +0000

    run pre-commit hooks format

--- getp-csrc/include/model.hpp
+++ getp-csrc/include/model.hpp
@@ -5,6 +5,7 @@
 #include "utils.hpp"
 #include "config.hpp"
 
-float *forward_gpu_20b(Config *p, OurTransformerWeights *weights, OurRunState *rs, int token, int pos);
-float *forward_cpu_20b(Config *p, OurTransformerWeights *weights, OurRunState *rs, int token, int pos);
-
+float *forward_gpu_20b(Config *p, OurTransformerWeights *weights, OurRunState *rs, int token,
+                       int pos);
+float *forward_cpu_20b(Config *p, OurTransformerWeights *weights, OurRunState *rs, int token,
+                       int pos);
